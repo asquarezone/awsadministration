7831b300eb54c4f8e561f8f9d000c6ca58ab455e
qtkhaja <qtkhaja@gmail.com>
2020-11-05 10:11:22 +0530

    added dynamic script

--- networking/nov20/awsvpc.sh
+++ networking/nov20/awsvpc.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 # Creating a vpc with aws cli
- aws ec2 create-vpc --cidr-block '10.100.0.0/16' --tag-specifications 'ResourceType=vpc,Tags=[{Key="Name",Value="ntier"}]'
+# aws ec2 create-vpc --cidr-block '10.100.0.0/16' --tag-specifications 'ResourceType=vpc,Tags=[{Key="Name",Value="ntier"}]'
 # vpc-id = vpc-06e51523894dde898
 
+vpcid=$(aws ec2 describe-vpcs --filter 'Name=cidr,Values=10.100.0.0/16' --query 'Vpcs[0].VpcId')
+
+
 # Create web subnet
-aws ec2 create-subnet --cidr-block '10.100.0.0/24'  --vpc-id 'vpc-06e51523894dde898' --tag-specifications 'ResourceType=subnet,Tags=[{Key="Name",Value="web"}]'
+aws ec2 create-subnet --cidr-block '10.100.0.0/24'  --vpc-id $vpcid --tag-specifications 'ResourceType=subnet,Tags=[{Key="Name",Value="web"}]'
 
 # Create app subnet
-aws ec2 create-subnet --cidr-block '10.100.1.0/24'  --vpc-id 'vpc-06e51523894dde898' --tag-specifications 'ResourceType=subnet,Tags=[{Key="Name",Value="app"}]'
+aws ec2 create-subnet --cidr-block '10.100.1.0/24'  --vpc-id "$vpcid" --tag-specifications 'ResourceType=subnet,Tags=[{Key="Name",Value="app"}]'
 
 # Create db subnet
-aws ec2 create-subnet --cidr-block '10.100.2.0/24'  --vpc-id 'vpc-06e51523894dde898' --tag-specifications 'ResourceType=subnet,Tags=[{Key="Name",Value="db"}]'
+aws ec2 create-subnet --cidr-block '10.100.2.0/24'  --vpc-id "$vpcid" --tag-specifications 'ResourceType=subnet,Tags=[{Key="Name",Value="db"}]'
 
 # Create mgmt subnet
-aws ec2 create-subnet --cidr-block '10.100.3.0/24'  --vpc-id 'vpc-06e51523894dde898' --tag-specifications 'ResourceType=subnet,Tags=[{Key="Name",Value="mgmt"}]'
+aws ec2 create-subnet --cidr-block '10.100.3.0/24'  --vpc-id "$vpcid" --tag-specifications 'ResourceType=subnet,Tags=[{Key="Name",Value="mgmt"}]'
