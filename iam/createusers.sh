88ba88fb003f11b6e550375a8132f0b3bd2d3b54
qtkhaja <qtkhaja@gmail.com>
2020-11-16 09:45:18 +0530

    Added iam

--- iam/createusers.sh
+++ iam/createusers.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+aws iam create-group --group-name 'avengers'
 for name in Ironman Gamora BlackPanther CaptianAmerica Thor Hulk BlackWidow groot nebula spiderman
 do
     aws iam create-user --user-name $name
     aws iam create-login-profile --user-name $name --password-reset-required --password 'india@123'
+    aws iam add-user-to-group --group-name 'avengers' --user-name $name
 done
+
