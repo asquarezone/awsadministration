98df757ead265eb524214060357d0aadc9375411
qtkhaja <qtkhaja@gmail.com>
2020-11-16 09:37:51 +0530

    Updated credential information

--- iam/createusers.sh
+++ iam/createusers.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-for name in BlackPanther CaptianAmerica Thor Hulk BlackWidow groot nebula spiderman
+for name in Ironman Gamora BlackPanther CaptianAmerica Thor Hulk BlackWidow groot nebula spiderman
 do
     aws iam create-user --user-name $name
+    aws iam create-login-profile --user-name $name --password-reset-required --password 'india@123'
 done
