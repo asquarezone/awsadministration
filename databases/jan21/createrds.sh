5888b40cdda3e0fd55c24e77db89800591cb382a
qtkhaja <qtkhaja@gmail.com>
2021-01-19 09:49:54 +0530

    Added describe commands

--- databases/jan21/createrds.sh
+++ databases/jan21/createrds.sh
@@ -2,4 +2,8 @@
 
 # This file will contain simple commands that can be executed to create rds
 # instances and configuring them
-aws rds create-db-instance --db-instance-identifier 'qtrdsfromcli' --allocated-storage 20 --db-instance-class 'db.t2.micro' --engine 'mysql' --master-username 'admin' --master-user-password 'admin1234' --vpc-security-group-ids "sg-075892da85bd09fc0" --backup-retention-period 7 --port 3306 --no-multi-az --no-auto-minor-version-upgrade 
+aws rds create-db-instance --db-instance-identifier 'qtrdsfromcli' --allocated-storage 20 --db-instance-class 'db.t2.micro' --engine 'mysql' --master-username 'admin' --master-user-password 'admin1234' --vpc-security-group-ids "sg-075892da85bd09fc0" --backup-retention-period 7 --port 3306 --no-multi-az --no-auto-minor-version-upgrade
+
+aws rds describe-db-instances
+
+aws rds describe-db-instances --db-instance-identifier 'qtrdsfromcli'
