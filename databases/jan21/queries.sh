005a943a8b41e7dcd756380cf6baecb585d2e54e
qtkhaja <qtkhaja@gmail.com>
2021-01-21 09:57:59 +0530

    Added queries

--- databases/jan21/queries.sh
+++ databases/jan21/queries.sh
@@ -16,5 +16,16 @@ aws rds describe-db-instances --query "DBInstances[*].{ID: DBInstanceIdentifier,
 # to fetch endpoint address for all mysql database instances
 aws rds describe-db-instances --query "DBInstances[?Engine=='mysql'].Endpoint.Address"
 
+#to fetch all the database snapshot identifiers and Status
+aws rds describe-db-snapshots --query "DBSnapshots[*].{Id: DBSnapshotIdentifier, DBId: DBInstanceIdentifier, Status: Status}"
+aws rds describe-db-snapshots --query "DBSnapshots[*].{Id: DBSnapshotIdentifier, DBId: DBInstanceIdentifier, Status: Status}" --output table
+
+# to fetch all the database snapshot identifiers and Snapshot Creation time for mysql engine
+aws rds describe-db-snapshots --query "DBSnapshots[?Engine=='mysql'].{Id: DBSnapshotIdentifier, DBId: DBInstanceIdentifier, CreationTime: SnapshotCreateTime}"
+
+
+# to fetch all the db subnet group names other than default
+aws rds describe-db-subnet-groups --query "DBSubnetGroups[?DBSubnetGroupName != 'default'].DBSubnetGroupName"
+
 
 
