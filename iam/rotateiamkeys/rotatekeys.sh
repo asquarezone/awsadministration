9a404c2439bf7ad66947946424bd1a7b8558deca
qtdevops <qtdevops@gmail.com>
2021-04-18 10:04:23 +0530

    Added script with functions

--- iam/rotateiamkeys/rotatekeys.sh
+++ iam/rotateiamkeys/rotatekeys.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
+
+### usage is get_access_key username
+get_access_key() {
+    access_key=$(aws iam list-access-keys --user-name $user --query "AccessKeyMetadata[0].AccessKeyId"|tr -d '"')
+    echo $access_key
+
+}
+
+send_email() {
+    echo "Sending email with json attached"
+}
+
 #users=$(aws iam list-users --query "Users[].UserName")
 users=(testuser1 testuser2)
 for user in ${users[@]}; do
   echo "rotating credentials for $user"
-  current_access_key=$(aws iam list-access-keys --user-name $user --query "AccessKeyMetadata[0].AccessKeyId")
-  current_access_key=$(echo $current_access_key | tr -d '"')
+  current_access_key=$(get_access_key $user)
   aws iam create-access-key --user-name $user > $user.json
-  # send email to admins or send json to users
+  send_email
   echo "deleting current access key $current_access_key"
   aws iam delete-access-key --access-key-id $current_access_key --user-name $user
-  new_access_key=$(aws iam list-access-keys --user-name $user --query "AccessKeyMetadata[0].AccessKeyId"|tr -d '"')
+  new_access_key=$(get_access_key $user)
   echo "New access key is $new_access_key"
 done
