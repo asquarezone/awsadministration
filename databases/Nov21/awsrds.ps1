747ee71113314bddf9fbe4f8acd206a5cf0e055e
qtpython <qtkhajacloud@gmail.com>
2021-11-09 09:37:49 +0530

    Added checking status of db created

--- databases/Nov21/awsrds.ps1
+++ databases/Nov21/awsrds.ps1
@@ -3,3 +3,8 @@ aws rds create-db-instance --db-instance-identifier 'qtrdsfromcli' `
     --master-username 'root' --master-user-password 'rootroot' `
     --publicly-accessible --db-name 'qtecommerce'  `
     --db-subnet-group-name 'qtsubnetgroup' --allocated-storage 20
+
+aws rds describe-db-instances --db-instance-identifier 'qtrdsfromcli' 
+
+aws rds describe-db-instances --db-instance-identifier 'qtrdsfromcli'`
+    --query 'DBInstances[0].DBInstanceStatus'
