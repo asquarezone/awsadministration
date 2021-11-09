22d90cc86b76453835917c0c8139706414e13de6
qtpython <qtkhajacloud@gmail.com>
2021-11-09 09:33:28 +0530

    Replace line extenders

--- databases/Nov21/awsrds.sh
+++ databases/Nov21/awsrds.sh
@@ -1,5 +1,5 @@
-aws rds create-db-instance --db-instance-identifier 'qtrdsfromcli' /
-    --db-instance-class 'db.t2.micro' --engine 'mysql' /
-    --master-username 'root' --master-user-password 'rootroot' /
-    --publicly-accessible --db-name 'qtecommerce'  /
+aws rds create-db-instance --db-instance-identifier 'qtrdsfromcli' \
+    --db-instance-class 'db.t2.micro' --engine 'mysql' \
+    --master-username 'root' --master-user-password 'rootroot' \
+    --publicly-accessible --db-name 'qtecommerce'  \
     --db-subnet-group-name 'qtsubnetgroup' --allocated-storage 20
