d355a79ef4cdae61dd350755079d7e1d2ea290d3
qtpython <qtkhajacloud@gmail.com>
2021-11-09 10:02:50 +0530

    Added multi az db

--- databases/Nov21/awsrds.sh
+++ databases/Nov21/awsrds.sh
@@ -14,3 +14,10 @@ aws rds describe-db-instances --db-instance-identifier 'qtrdsfromcli' \
 
 aws rds delete-db-instance --db-instance-identifier 'qtrdsfromcli' \
     --skip-final-snapshot --delete-automated-backups
+
+aws rds create-db-instance --db-instance-identifier 'qtrdsfromclimulti' \
+    --db-instance-class 'db.t2.micro' --engine 'mysql' \
+    --master-username 'root' --master-user-password 'rootroot' \
+    --publicly-accessible --db-name 'qtecommerce'  \
+    --db-subnet-group-name 'qtsubnetgroup' --allocated-storage 20 \
+    --multi-az --vpc-security-group-ids 'sg-0c8eee8f2fbefd89e'
