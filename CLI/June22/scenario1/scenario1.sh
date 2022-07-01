cefdf30efd311e58e20045cd10962e5d26fed54e
shaikkhajaibrahim <qtkhajacloud@gmail.com>
2022-07-01 10:01:54 +0530

    Added shell script fixes

--- CLI/June22/scenario1/scenario1.sh
+++ CLI/June22/scenario1/scenario1.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 vpc_cidr='192.168.0.0/16'
-$vpc_id=$(aws ec2 create-vpc --cidr-block $vpc_cidr --query "Vpc.VpcId" --tag-specifications "ResourceType=vpc,Tags=[{Key=Name,Value=ntier}]" --output text)
+vpc_id=$(aws ec2 create-vpc --cidr-block $vpc_cidr --query "Vpc.VpcId" --tag-specifications "ResourceType=vpc,Tags=[{Key=Name,Value=ntier}]" --output text)
 echo "Vpc with cidr ${vpc_cidr} is created with id ${vpc_id}"
 
-az_a_subnets=('web1', 'app1', 'db1')
-az_a_subnets_cidrs=('192.168.0.0/24', '192.168.2.0/24', '192.168.4.0/24')
+az_a_subnets=('web1' 'app1' 'db1')
+az_a_subnets_cidrs=('192.168.0.0/24' '192.168.2.0/24' '192.168.4.0/24')
 
 for index in ${!az_a_subnets[@]}; do
   subnet_name=${az_a_subnets[$index]}
   aws ec2 create-subnet --vpc-id $vpc_id --availability-zone "us-west-2a" --cidr-block ${az_a_subnets_cidrs[$index]} --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=$subnet_name}]"
 done 
 
-az_b_subnets=('web2', 'app2', 'db2')
-az_b_subnets_cidrs=('192.168.1.0/24', '192.168.3.0/24', '192.168.5.0/24')
+az_b_subnets=('web2' 'app2' 'db2')
+az_b_subnets_cidrs=('192.168.1.0/24' '192.168.3.0/24' '192.168.5.0/24')
 
 for index in ${!az_b_subnets[@]}; do
   subnet_name=${az_b_subnets[$index]}
