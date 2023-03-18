9c46486365f844ed1f1ce7d28291efb3fc6a6e8c
Vishnuchowki <125731972+Vishnuchowki@users.noreply.github.com>
2023-03-18 12:27:38 +0530

    Update activity1.sh

--- CLI/Mar23/workshop/activity1.sh
+++ CLI/Mar23/workshop/activity1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-instanceIds=$(aws ec2 describe-instances --filters "Name=tag:Env,Values=QA" "Name=instance-state-name,Values=running" --query "Reservations[].Instances[].InstanceId" --output text)
+instanceIds=$(aws ec2 describe-instances --filters "Name=tag:Env,Values=DEV" "Name=instance-state-name,Values=running" --query "Reservations[].Instances[].InstanceId" --output text)
 
 if [[ -n $instanceIds ]]; then
     echo "The instance ids which will be shutdown are ${instanceIds}"
     aws ec2 stop-instances --instance-ids ${instanceIds}
 else
-    echo "No instances found with matching criteria Env = QA"
+    echo "No instances found with matching criteria Env = DEV"
 fi
