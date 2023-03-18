2ad90adae3da28e2e35942be55c79fc34c0f2788
shaikkhajaibrahim <qtkhajacloud@gmail.com>
2023-03-18 12:23:22 +0530

    Added fix for script

--- CLI/Mar23/workshop/activity1.sh
+++ CLI/Mar23/workshop/activity1.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 instanceIds=$(aws ec2 describe-instances --filters "Name=tag:Env,Values=QA" "Name=instance-state-name,Values=running" --query "Reservations[].Instances[].InstanceId" --output text)
-echo "The instance ids which will be shutdown are ${instanceIds}"
-aws ec2 stop-instances --instance-ids ${instanceIds}
+
+if [[ -n $instanceIds ]]; then
+    echo "The instance ids which will be shutdown are ${instanceIds}"
+    aws ec2 stop-instances --instance-ids ${instanceIds}
+else
+    echo "No instances found with matching criteria Env = QA"
+fi
