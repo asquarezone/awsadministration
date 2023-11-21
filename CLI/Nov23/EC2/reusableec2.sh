afab063464cc8c55319f6ebad7bd996ff003164e
qtdevops <qtdevops@gmail.com>
2023-11-21 10:28:55 +0530

    Added guard to fail if number of arguments are not 7

--- CLI/Nov23/EC2/reusableec2.sh
+++ CLI/Nov23/EC2/reusableec2.sh
@@ -2,6 +2,12 @@
 
 #./reusableec2.sh "us-east-1" "vpc-cd32bcb0" "subnet-15ede058" "sg-0bdd39a6be616d961 sg-0f67708d44b7f6cb1" "id_rsa" "ami-0fc5d935ebf8bc3bc" "t2.micro"
 
+if [[ $# -ne 7 ]]; then
+    echo "This script needs 7 arguments"
+    echo ./reusableec2.sh "<region>" "<vpc-id>" "<subnet-id>" "<security-group-id>" "<key-name>" "<ami-id>" "<instance-type>"
+    exit 1
+fi
+
 REGION=$1
 #"us-east-1"
 AZ="${REGION}a"
@@ -25,6 +31,14 @@ AMI_ID=$6
 #"ami-0fc5d935ebf8bc3bc"
 INSTANCE_TYPE=$7
 #"t2.micro"
+
+echo "aws ec2 run-instances \
+    --instance-type ${INSTANCE_TYPE} \
+    --key-name ${KEY_NAME} \
+    --security-group-ids ${SECURITY_GROUP_IDS} \
+    --subnet-id ${SUBNET_ID} \
+    --image-id ${AMI_ID} \
+"
 
 
 # Create an ec2 instance
