053822c7c77aa53f9bd63891850835123852f19a
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-04-04 09:50:18 +0530

    Added support for region

---
+++ CLI/Apr24/createec2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Author: Khaja
+# Description: This script creates ec2 instance depending on arguments passed
+#
+# Usage: ./create_ec2.sh <instance_type> <ami_id> <key_name> <security_group_id>
+# Example: ./create_ec2.sh t2.micro ami-007020fd9c84e18c7 docker sg-05c068fa4a48f13e0
--- CLI/Apr24/createvpc.sh
+++ CLI/Apr24/createvpc.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+REGION="ap-south-1"
+
 # create vpc
 vpc_id=$(aws ec2 create-vpc --cidr-block "192.168.0.0/16" \
     --tag-specifications "ResourceType=vpc,Tags=[{Key=Name,Value=learning}]" \
     --query "Vpc.VpcId" \
-    --output text)
+    --output text \
+    --region ${REGION} \
+    )
 
 echo "VPC ID: ${vpc_id}"
 
@@ -13,8 +17,9 @@ web1_subnet=$(aws ec2 create-subnet \
     --vpc-id ${vpc_id} \
     --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=web-1}]" \
     --cidr-block "192.168.0.0/24" \
-    --availability-zone "ap-south-1a" \
+    --availability-zone "${REGION}a" \
     --output text \
+    --region ${REGION} \
     --query "Subnet.SubnetId")
 
 echo "Web 1 Subnet ID: ${web1_subnet}"
@@ -23,8 +28,9 @@ web2_subnet=$(aws ec2 create-subnet \
     --vpc-id ${vpc_id} \
     --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=web-2}]" \
     --cidr-block "192.168.1.0/24" \
-    --availability-zone "ap-south-1b" \
+    --availability-zone "${REGION}b" \
     --output text \
+    --region ${REGION} \
     --query "Subnet.SubnetId")
 
 echo "Web 2 Subnet ID: ${web2_subnet}"
@@ -34,8 +40,9 @@ db1_subnet=$(aws ec2 create-subnet \
     --vpc-id ${vpc_id} \
     --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=db-1}]" \
     --cidr-block "192.168.2.0/24" \
-    --availability-zone "ap-south-1a" \
+    --availability-zone "${REGION}a" \
     --output text \
+    --region ${REGION} \
     --query "Subnet.SubnetId")
 
 echo "DB 1 Subnet ID: ${db1_subnet}"
@@ -46,8 +53,9 @@ db2_subnet=$(aws ec2 create-subnet \
     --vpc-id ${vpc_id} \
     --tag-specifications "ResourceType=subnet,Tags=[{Key=Name,Value=db-2}]" \
     --cidr-block "192.168.3.0/24" \
-    --availability-zone "ap-south-1b" \
+    --availability-zone "${REGION}b" \
     --output text \
+    --region ${REGION} \
     --query "Subnet.SubnetId")
 
 echo "DB 2 Subnet ID: ${db2_subnet}"
