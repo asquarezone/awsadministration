2f953ce9ea9bd64e0763d57ff75559af52f226b2
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-11-10 09:35:35 +0530

    Added azure sql creation

--- CLI/Nov24/db/createazsql.sh
+++ CLI/Nov24/db/createazsql.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 resourceGroup='dbfromcli'
 location='eastus'
 
@@ -9,3 +11,45 @@ if [ $(az group exists --name $resourceGroup) = false ]; then
 else
    echo "$resourceGroup already exists"
 fi
+
+tag="create-and-configure-database"
+server="ltsqlfromcli"
+database="library"
+login="ltdevops"
+password="motherindia@123"
+# Specify appropriate IP address values for your environment
+# to limit access to the SQL Database server
+startIp=0.0.0.0
+endIp=0.0.0.0
+
+echo "Using resource group $resourceGroup with login: $login, password: $password..."
+
+echo "Creating $server in $location..."
+az sql server create \
+   --name $server \
+   --resource-group $resourceGroup \
+   --location "$location" \
+   --admin-user $login \
+   --admin-password $password
+echo "Configuring firewall..."
+az sql server firewall-rule create \
+   --resource-group $resourceGroup \
+   --server $server \
+   -n AllowYourIp \
+   --start-ip-address $startIp \
+   --end-ip-address $endIp
+echo "Creating $database in serverless tier"
+az sql db create \
+   --resource-group $resourceGroup \
+   --server $server \
+   --name $database \
+   --sample-name AdventureWorksLT \
+   --edition GeneralPurpose \
+   --compute-model Serverless \
+   --family Gen5 \
+   --capacity 2
+
+
+az group delete \
+   --name $resourceGroup \
+   --yes
