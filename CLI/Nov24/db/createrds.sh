e27eb758e2b6a5cae3848b477fac952c6763a573
shaikkhajaibrahim <qtkhajadevops@gmail.com>
2024-11-10 09:22:15 +0530

    Added changes

--- CLI/Nov24/db/createrds.sh
+++ CLI/Nov24/db/createrds.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -ex
 # ensure security group is present
 SECURITY_GROUP_NAME=rds-mysql
 
@@ -12,3 +12,44 @@ else
     echo "Create a security group with name $SECURITY_GROUP_NAME"
     exit 1
 fi
+# get the security group id
+sg_id=$(aws ec2 describe-security-groups --filters "Name=group-name, Values=$SECURITY_GROUP_NAME" --query "SecurityGroups[0].GroupId" --output=text)
+
+engine='mysql'
+db_id='ltrdslibrarydb'
+size=db.t3.micro
+username=qtdevops
+password=qtdevops123
+storage=20
+
+aws rds create-db-instance \
+    --db-name 'library' \
+    --db-instance-identifier $db_id \
+    --db-instance-class $size \
+    --master-username $username \
+    --master-user-password $password \
+    --vpc-security-group-ids $sg_id \
+    --engine $engine \
+    --allocated-storage $storage
+
+
+# wait for the db instance to be available
+aws rds wait db-instance-available --db-instance-identifier $db_id
+
+
+# create a database snapshot
+aws rds create-db-snapshot \
+    --db-instance-identifier $db_id \
+    --db-snapshot-identifier "$db_id-snapshot-1"
+
+# wait for the snapshot to be available
+aws rds wait db-snapshot-available --db-snapshot-identifier "$db_id-snapshot-1"
+
+# delete the db instance
+aws rds delete-db-instance \
+    --db-instance-identifier $db_id \
+    --skip-final-snapshot
+
+
+
+
